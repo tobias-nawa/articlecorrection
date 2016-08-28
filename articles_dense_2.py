395aa962c0acbba471559d292963e0531875e64f
Tobias Nawa <kontakt@tobias-nawa.de>
2016-08-28 17:10:06 +0200

    6 inputs: 20k hidden, output

--- articles_dense_2.py
+++ articles_dense_2.py
@@ -32,8 +32,8 @@ batch_size = 256
 nb_epoch = 25
 nb_classes = 4 # a, an, the, none
 validation_split = 0.2
-loss = 'categorical_crossentropy'
-optim = 'adam'
+loss = 'mean_squared_error'
+optim = 'sgd'
 
 exclude = set(string.punctuation)
 
